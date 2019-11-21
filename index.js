3f54aa9d8bd08dcb0895c505b8f80aa39b949260
Tom Spencer <tom.spencer@studiozeffa.com>
2019-11-21 14:09:11 +0000

    Use in-memory adapter

--- index.js
+++ index.js
@@ -2,24 +2,14 @@ const TABLE_NAME = 'knex_bindings_test';
 
 const knex = require('knex')({
   client: 'sqlite3',
-  connection: {
-    filename: './mydb.sqlite'
-  }
-});
-
-main().catch(err => {
-  console.error(err);
-  process.exit(1);
+  connection: ':memory:'
 });
 
 async function main() {
-  const hasTable = await knex.schema.hasTable(TABLE_NAME);
-  if(!hasTable) {
-    await knex.schema.createTable(TABLE_NAME, table => {
-      table.string('id').primary();
-      table.json('data');
-    });
-  }
+  await knex.schema.createTable(TABLE_NAME, table => {
+    table.string('id').primary();
+    table.json('data');
+  });
 
   const insert = knex(TABLE_NAME).insert({
     id: '1',
@@ -31,3 +21,8 @@ async function main() {
 
   await knex.raw('?', [insert]);
 }
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
